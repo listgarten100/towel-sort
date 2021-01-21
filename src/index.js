000064fdf459c3a21bf3484402fa75bd4827211c
Vlad Listgarten <listgarten100@gmail.com>
2021-01-21 23:15:32 +0200

    feat: create function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 //start task
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix) {
+    return [];
+  }
+  let arr = [];
+  matrix.forEach((elem, i) => {
+    if(i % 2 !== 0) {
+      elem.reverse();
+    }
+    elem.forEach(elem => {
+      arr.push(elem);
+    });
+  });
+  return arr;
 };
